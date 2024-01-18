1cd990a8a21cfd1d071bca8c63a003d761ee1974
rookedsysc <rookedsysc36@gmail.com>
2024-01-18 19:48:30 +0900

    first commit

---
+++ src/test/java/com/gamemoonchul/GamemoonchulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamemoonchul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamemoonchulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
