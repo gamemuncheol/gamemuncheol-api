c81f4ca29e3fb2cdec6b4312750110fe25d0cb1e
rookedsysc <rookedsysc36@gmail.com>
2024-02-08 20:52:28 +0900

    feat Apple SignUp Service 개발 Token을 받아와서 검증하고 User 있는지 체크 후 Repository에 저장

--- src/main/java/com/gamemoonchul/application/AppleService.java
+++ src/main/java/com/gamemoonchul/application/AppleService.java
@@ -1,7 +1,9 @@
 package com.gamemoonchul.application;
 
+import com.gamemoonchul.common.exception.ApiException;
 import com.gamemoonchul.config.apple.AppleIDTokenValidator;
 import com.gamemoonchul.config.apple.entities.AppleUserInfo;
+import com.gamemoonchul.config.apple.enums.AppleTokenStatus;
 import com.gamemoonchul.config.jwt.TokenDto;
 import com.gamemoonchul.config.jwt.TokenHelper;
 import com.gamemoonchul.domain.MemberEntity;
@@ -10,6 +12,8 @@ import com.gamemoonchul.infrastructure.repository.MemberRepository;
 import com.gamemoonchul.infrastructure.web.dto.AppleSignUpRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,12 +24,22 @@ public class AppleService {
 
   public TokenDto signUp(AppleSignUpRequestDto signUpRequest) {
     AppleUserInfo appleUserInfo = appleIDTokenValidator.extractAppleUserinfoFromIDToken(signUpRequest.getIdentityToken());
+    if(signUpRequest.getName() == null || signUpRequest.getName().isEmpty()) {
+      throw new ApiException(AppleTokenStatus.INVALID_SIGNUP_FORM);
+    }
     appleUserInfo.setName(signUpRequest.getName());
 
-    MemberEntity member = MemberConverter.toEntity(appleUserInfo);
-    memberRepository.save(member);
+    Optional<MemberEntity> alreadyExistMember = memberRepository.findTop1ByEmail(appleUserInfo.getEmail());
+    MemberEntity member = new MemberEntity();
 
+    if (alreadyExistMember.isEmpty()) {
+      member = MemberConverter.toEntity(appleUserInfo);
+      memberRepository.save(member);
+    } else {
+      member = alreadyExistMember.get();
+    }
     TokenDto token  = tokenHelper.createToken(member.getEmail());
+
     return token;
   }
 }
