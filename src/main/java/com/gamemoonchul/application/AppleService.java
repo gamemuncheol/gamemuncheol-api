645d2cc49856b5c1bbafe808fdef5ea934c3933e
rookedsysc <rookedsysc36@gmail.com>
2024-02-08 21:23:44 +0900

    feat Apple Service에서 MemberRepository로 바로 접근하지 않게 수정

--- src/main/java/com/gamemoonchul/application/AppleService.java
+++ src/main/java/com/gamemoonchul/application/AppleService.java
@@ -8,18 +8,16 @@ import com.gamemoonchul.config.jwt.TokenDto;
 import com.gamemoonchul.config.jwt.TokenHelper;
 import com.gamemoonchul.domain.MemberEntity;
 import com.gamemoonchul.domain.converter.MemberConverter;
-import com.gamemoonchul.infrastructure.repository.MemberRepository;
 import com.gamemoonchul.infrastructure.web.dto.AppleSignUpRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AppleService {
   private final AppleIDTokenValidator appleIDTokenValidator;
-  private final MemberRepository memberRepository;
+  private final MemberService memberService;
   private final TokenHelper tokenHelper;
 
   public TokenDto signUp(AppleSignUpRequestDto signUpRequest) {
@@ -28,18 +26,9 @@ public class AppleService {
       throw new ApiException(AppleTokenStatus.INVALID_SIGNUP_FORM);
     }
     appleUserInfo.setName(signUpRequest.getName());
-
-    Optional<MemberEntity> alreadyExistMember = memberRepository.findTop1ByEmail(appleUserInfo.getEmail());
-    MemberEntity member = new MemberEntity();
-
-    if (alreadyExistMember.isEmpty()) {
-      member = MemberConverter.toEntity(appleUserInfo);
-      memberRepository.save(member);
-    } else {
-      member = alreadyExistMember.get();
-    }
+    MemberEntity member = MemberConverter.toEntity(appleUserInfo);
+    memberService.signInOrUp(member);
     TokenDto token  = tokenHelper.createToken(member.getEmail());
-
     return token;
   }
 }
