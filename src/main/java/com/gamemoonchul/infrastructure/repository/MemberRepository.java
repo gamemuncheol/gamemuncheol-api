d0c72d4eb3953132ddec4690f385e8f41695e088
rookedsysc <rookedsysc36@gmail.com>
2024-02-08 22:28:37 +0900

    refactor Test를 위해서 AppleUserInfo 및 MemberRepository 리팩터링

--- src/main/java/com/gamemoonchul/infrastructure/repository/MemberRepository.java
+++ src/main/java/com/gamemoonchul/infrastructure/repository/MemberRepository.java
@@ -3,9 +3,13 @@ package com.gamemoonchul.infrastructure.repository;
 
 import com.gamemoonchul.domain.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository  extends JpaRepository<MemberEntity, Long> {
   Optional<MemberEntity> findTop1ByEmail(String email);
+  List<MemberEntity> findByEmail(String email);
 }
