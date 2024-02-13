8258869eb84940b726456df6a572624abec5ae13
rookedsysc <rookedsysc36@gmail.com>
2024-02-14 04:55:38 +0900

    refactor Searched Model VO -> DTO 변경

---
+++ src/main/java/com/gamemoonchul/domain/dto/SearchedUser.java
@@ -0,0 +1,17 @@
+package com.gamemoonchul.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SearchedUser {
+    private String nickname;
+    @JsonProperty("img_src")
+    private String imgSrc;
+}
