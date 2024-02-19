718409c29a6cf2f959e710093230826adf5889b9
rookedsysc <rookedsysc36@gmail.com>
2024-02-20 06:19:25 +0900

    feat CustomEntryPoint 설정해서 Redirect 방지

--- src/main/java/com/gamemoonchul/config/SpringSecurityConfig.java
+++ src/main/java/com/gamemoonchul/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.gamemoonchul.config;
 
 import com.gamemoonchul.config.jwt.JwtAuthorizationFilter;
 import com.gamemoonchul.config.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.gamemoonchul.config.oauth.CustomAuthenticationEntryPoint;
 import com.gamemoonchul.config.oauth.handler.OAuth2AuthenticationFailureHandler;
 import com.gamemoonchul.config.oauth.handler.OAuth2AuthenticationSuccessHandler;
 import com.gamemoonchul.config.oauth.service.CustomOAuth2UserService;
@@ -25,55 +26,60 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
-  private List<String> SWAGGER = List.of("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/v3/api-docs/**");
-  //private List<String> EXCEPTION = List.of("/test/**");
-  private List<String> EXCEPTION = List.of("/open-api/**");
+    private List<String> SWAGGER = List.of("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/v3/api-docs/**");
+    //private List<String> EXCEPTION = List.of("/test/**");
+    private List<String> EXCEPTION = List.of("/open-api/**");
 
-  private final CustomOAuth2UserService customOauth;
-  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-  private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-  private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
-  private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final CustomOAuth2UserService customOauth;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(req -> {
+                    // static Resource에 대해서는 모두 허가함
+                    req.requestMatchers(
+                                    PathRequest.toStaticResources()
+                                            .atCommonLocations()
+                            )
+                            .permitAll()
+                            .requestMatchers(SWAGGER.toArray(new String[0]))
+                            .permitAll()
+                            .requestMatchers(EXCEPTION.toArray(new String[0]))
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated()
+                    ;
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // For H2 DB
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // session 사용하지 않도록 설정
+                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(configure ->
+                        configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
+                                .userInfoEndpoint(config -> config.userService(customOauth))
+                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                                .failureHandler(oAuth2AuthenticationFailureHandler)
+                );
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(it -> {
-          // static Resource에 대해서는 모두 허가함
-          it.requestMatchers(
-                  PathRequest.toStaticResources()
-                      .atCommonLocations()
-              )
-              .permitAll()
-              .requestMatchers(SWAGGER.toArray(new String[0]))
-              .permitAll()
-              .requestMatchers(EXCEPTION.toArray(new String[0]))
-              .permitAll()
-              .anyRequest()
-              .authenticated()
-          ;
-        })
-        .csrf(AbstractHttpConfigurer::disable)
-        .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // For H2 DB
-        .formLogin(AbstractHttpConfigurer::disable)
-        .httpBasic(AbstractHttpConfigurer::disable)
-        // session 사용하지 않도록 설정
-        .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2Login(configure ->
-            configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
-                .userInfoEndpoint(config -> config.userService(customOauth))
-                .successHandler(oAuth2AuthenticationSuccessHandler)
-                .failureHandler(oAuth2AuthenticationFailureHandler)
+        /**
+         * Exception 발생시 Redirect를 하지 않고 401을 반환
+         */
+        http.exceptionHandling(
+                exceptionHandlingConfigurer -> exceptionHandlingConfigurer.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
         );
 
-    http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
-  }
-
+        return http.build();
+    }
 }
