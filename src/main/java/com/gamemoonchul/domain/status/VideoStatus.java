a84c4c7818f29f0ffdaea4d2131d7868552e543f
rookedsysc <rookedsysc36@gmail.com>
2024-03-13 02:57:49 +0900

    feat S3 업로드 개발

--- src/main/java/com/gamemoonchul/domain/status/VideoStatus.java
+++ src/main/java/com/gamemoonchul/domain/status/VideoStatus.java
@@ -14,7 +14,9 @@ public enum VideoStatus implements ApiStatusIfs {
     INVALID_FILETYPE(4400, "지원하지 않는 파일 형식입니다."),
     FILE_SIZE_EXCEEDED(4401, "파일 크기가 최대 크기를 초과하였습니다."),
     FILE_UPLOAD_FAILED(4502, "파일 업로드에 실패하였습니다."),
+    S3_UPLOAD_FAILED(4503, "S3 업로드에 실패하였습니다."),
     EMPTY_FILE(4403, "파일이 비어있습니다."),
+    UNKOWN_ERROR(4504, "알 수 없는 에러가 발생하였습니다."),
     ;
     private final Integer statusCode;
   private final String message;
