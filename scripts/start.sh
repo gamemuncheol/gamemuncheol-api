23872d39af8f160e63b926bd11cbdfc2fccc7925
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-03-24 15:00:26 +0900

    Code Deploy Error Fix (#51)
    * fix CodeDeploy Error Msg Detail :
    The overall deployment failed because too many individual instances failed deployment, too few healthy instances are available for deployment, or some instances in your deployment group are experiencing problems.
    
    * fix Typo
    
    * fix Unable to access jarfile /home/ubuntu/spring-github-action/application.jar 에러 수정
    
    * fix Error: Unable to access jarfile /home/ubuntu/spring-github-action/home/ubuntu/spring-github-action/gamemoonchul-0.0.1-SNAPSHOT.jar Error Fix
    
    * fix stop.log > Service Not Found Fix
    
    * fix java.lang.IllegalStateException: Could not load store from 'classpath:keystore-aws.p12' Error Fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
-JAR="$ROOT_PATH/application.jar"
+JAR="$ROOT_PATH/gamemoonchul-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
@@ -9,8 +9,6 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
-JAR="$ROOT_PATH/application.jar"
+JAR="$ROOT_PATH/gamemoonchul-0.0.1-SNAPSHOT.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NouFound" >> $STOP_LOG
+  echo "서비스 NotFound" >> $STOP_LOG
 else
   echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
