3ffe527bdafab469a5d2fa6b3bc094683174523b
rookedsysc <rookedsysc36@gmail.com>
2024-04-22 03:13:34 +0900

    modify 이미지, 비디오 업로드 API Riot -> Post로 이동

--- src/main/java/com/gamemoonchul/infrastructure/web/PostApiController.java
+++ src/main/java/com/gamemoonchul/infrastructure/web/PostApiController.java
@@ -1,6 +1,7 @@
 package com.gamemoonchul.infrastructure.web;
 
 import com.gamemoonchul.application.PostService;
+import com.gamemoonchul.application.S3Service;
 import com.gamemoonchul.common.annotation.MemberSession;
 import com.gamemoonchul.domain.entity.Member;
 import com.gamemoonchul.infrastructure.web.common.RestControllerWithEnvelopPattern;
@@ -8,12 +9,14 @@ import com.gamemoonchul.infrastructure.web.dto.PostResponseDto;
 import com.gamemoonchul.infrastructure.web.dto.PostUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestControllerWithEnvelopPattern
 @RequestMapping("/api/post")
 @RequiredArgsConstructor
 public class PostApiController {
     private final PostService postService;
+    private final S3Service s3Service;
 
     @PostMapping("/upload")
     public PostResponseDto upload(
@@ -31,4 +34,15 @@ public class PostApiController {
         return postService.delete(id, member);
     }
 
+    @PostMapping("/video-upload")
+    public String uploadVideo(@RequestParam("file") MultipartFile file) {
+        String fileUrl = s3Service.uploadVideo(file);
+        return fileUrl;
+    }
+
+    @PostMapping("/thumbnail-upload")
+    public String uploadThumbnail(@RequestParam("file") MultipartFile file) {
+        String fileUrl = s3Service.uploadImage(file);
+        return fileUrl;
+    }
 }
--- src/main/java/com/gamemoonchul/infrastructure/web/RiotApiController.java
+++ src/main/java/com/gamemoonchul/infrastructure/web/RiotApiController.java
@@ -13,23 +13,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/riot")
 public class RiotApiController {
     private final RiotApiService riotApiService;
-    private final S3Service s3Service;
 
     @GetMapping("/search-match/{gameId}")
     public MatchGameResponse searchMatch(@PathVariable String gameId) {
         return riotApiService.searchMatch("KR_" + gameId);
     }
-
-    @PostMapping("/video-upload")
-    public String uploadVideo(@RequestParam("file") MultipartFile file) {
-        String fileUrl = s3Service.uploadVideo(file);
-        return fileUrl;
-    }
-
-    @PostMapping("/thumbnail-upload")
-    public String uploadThumbnail(@RequestParam("file") MultipartFile file) {
-        String fileUrl = s3Service.uploadImage(file);
-        return fileUrl;
-    }
-
 }
