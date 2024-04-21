b3c125cd31918ede56cf247f7de5a8e79ebd0342
rookedsysc <rookedsysc36@gmail.com>
2024-04-22 04:50:00 +0900

    feat Pagination DTO 생성

---
+++ src/main/java/com/gamemoonchul/infrastructure/web/common/Pagination.java
@@ -0,0 +1,26 @@
+package com.gamemoonchul.infrastructure.web.common;
+
+import lombok.*;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@Getter
+public class Pagination<T> {
+    private Integer page; // 몇 번째 페이지를 보여줄지
+    private Integer size; // 한 페이지에 보여줄 elements 수
+    private Integer currentElements; // 현재 return된 elements 수
+    private Integer totalPages; // 최대 페이지
+    private Long totalElements; // 전체 elements 수
+    private List<T> data;
+
+    public Pagination(Page<T> page) {
+        this.page = page.getNumber();
+        this.size = page.getSize();
+        this.currentElements = page.getNumberOfElements();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.data = page.getContent();
+    }
+}
