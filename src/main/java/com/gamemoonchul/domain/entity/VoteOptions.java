17563f2932feadfc05cd9fc1b301caf457744e46
slbin-park <smpts00@naver.com>
2024-05-15 22:12:25 +0900

    feat: voteOptions 와 연관관계 매핑

--- src/main/java/com/gamemoonchul/domain/entity/VoteOptions.java
+++ src/main/java/com/gamemoonchul/domain/entity/VoteOptions.java
@@ -7,18 +7,26 @@ import lombok.*;
 import java.util.List;
 
 
-@Builder @Getter
+@Builder
+@Getter
 @Entity(name = "VOTE_OPTIONS")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class VoteOptions {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "postId")
     private Post post;
 
-    @OneToOne(fetch = FetchType.LAZY) @JoinColumn(name = "matchUserId")
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "matchUserId")
     private MatchUser matchUser;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voteOptionsId")
+    private List<Vote> votes;
+
 }
