16fa93874071545827b29eb5b42ad90d3f802e6b
rookedsysc <rookedsysc36@gmail.com>
2024-05-19 06:36:42 +0900

    feat: CommentFixRequest to Entity 개발

--- src/main/java/com/gamemoonchul/application/converter/CommentConverter.java
+++ src/main/java/com/gamemoonchul/application/converter/CommentConverter.java
@@ -5,15 +5,20 @@ import com.gamemoonchul.domain.entity.Comment;
 import com.gamemoonchul.domain.entity.Member;
 import com.gamemoonchul.domain.entity.Post;
 import com.gamemoonchul.domain.status.PostStatus;
+import com.gamemoonchul.infrastructure.repository.CommentRepository;
 import com.gamemoonchul.infrastructure.repository.PostRepository;
+import com.gamemoonchul.infrastructure.web.dto.CommentFixRequest;
 import com.gamemoonchul.infrastructure.web.dto.CommentRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.gamemoonchul.domain.entity.QPost.post;
 
 @Service
 @RequiredArgsConstructor
 public class CommentConverter {
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
     public Comment requestToEntity(Member member, CommentRequest request) {
         Post post = postRepository.findById(request.postId())
@@ -27,5 +32,14 @@ public class CommentConverter {
                 .post(post)
                 .content(request.content())
                 .build();
+    }
+
+    public Comment requestToEntity(CommentFixRequest request) {
+        Comment savedComment = commentRepository.findById(request.commentId())
+                .orElseThrow(() ->
+                        new ApiException(PostStatus.COMMENT_NOT_FOUND)
+                );
+        savedComment.setContent(request.contents());
+        return savedComment;
     }
 }
