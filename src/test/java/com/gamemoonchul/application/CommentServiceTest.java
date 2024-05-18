d827ec3541c8f14c95622b554fa6066602739e32
rookedsysc <rookedsysc36@gmail.com>
2024-05-19 06:37:31 +0900

    feat: CommentServiceTest 추가

--- src/test/java/com/gamemoonchul/application/CommentServiceTest.java
+++ src/test/java/com/gamemoonchul/application/CommentServiceTest.java
@@ -4,16 +4,16 @@ import com.gamemoonchul.TestDataBase;
 import com.gamemoonchul.common.exception.ApiException;
 import com.gamemoonchul.domain.entity.*;
 import com.gamemoonchul.domain.status.PostStatus;
+import com.gamemoonchul.infrastructure.repository.CommentRepository;
 import com.gamemoonchul.infrastructure.repository.MemberRepository;
 import com.gamemoonchul.infrastructure.repository.PostRepository;
+import com.gamemoonchul.infrastructure.web.dto.CommentFixRequest;
 import com.gamemoonchul.infrastructure.web.dto.CommentRequest;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
 
 class CommentServiceTest extends TestDataBase {
     @Autowired
@@ -23,28 +23,58 @@ class CommentServiceTest extends TestDataBase {
     @Autowired
     private CommentService commentService;
     @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
     private EntityManager em;
+
+    Post post;
+    Member member;
+
+    @BeforeEach
+    void setUp() {
+        post = PostDummy.createPost("Hi");
+        post = postRepository.save(post);
+        member = MemberDummy.create();
+        member = memberRepository.save(member);
+    }
 
     @Test
     @DisplayName("코멘트 저장이 잘 되는지, post의 comment 개수가 정상적으로 업데이트 되는지 테스트")
     void saveTest() {
         // given
-        Post post = PostDummy.createPost("Hi");
-        post = postRepository.save(post);
-        Member member = MemberDummy.create();
-        member = memberRepository.save(member);
         CommentRequest request = CommentDummy.createRequest(post.getId());
         em.clear(); // 1차 캐쉬 제거
 
         // when
         Comment savedEntity = commentService.save(request, member);
-        Post searchedPost = postRepository.findById(post.getId()).orElseThrow(
-                () -> new ApiException(PostStatus.POST_NOT_FOUND)
-        );
+        Post searchedPost = postRepository.findById(post.getId())
+                .orElseThrow(
+                        () -> new ApiException(PostStatus.POST_NOT_FOUND)
+                );
 
         // then
         assertThat(searchedPost.getCommentCount()).isEqualTo(post.getCommentCount() + 1);
         assertThat(savedEntity.getContent()).isEqualTo(request.content());
-        assertThat(savedEntity.getPost().getId()).isEqualTo(request.postId());
+        assertThat(savedEntity.getPost()
+                .getId()).isEqualTo(request.postId());
+    }
+
+    @Test
+    @DisplayName("코멘트 저장 후 수정시 ID는 그대로 인데 contents 내용이 수정이 되는지 테스트")
+    void fixTest() throws InterruptedException {
+        // given
+        Comment savedEntity = commentRepository.save(CommentDummy.create(post, member));
+        CommentFixRequest content = CommentFixRequest.builder()
+                .contents("new contents")
+                .commentId(savedEntity.getId())
+                .build();
+        em.clear();
+
+        // when
+        Comment fixedComment = commentService.fix(content, member);
+
+        // then
+        assertThat(savedEntity.getContent()).isNotEqualTo(fixedComment.getContent());
+        assertThat(savedEntity.getId()).isEqualTo(fixedComment.getId());
     }
 }
--- src/test/java/com/gamemoonchul/domain/entity/CommentDummy.java
+++ src/test/java/com/gamemoonchul/domain/entity/CommentDummy.java
@@ -6,4 +6,12 @@ public class CommentDummy {
     public static CommentRequest createRequest(Long postId) {
         return new CommentRequest("content", postId);
     }
+
+    public static Comment create(Post post, Member member) {
+        return Comment.builder()
+                .member(member)
+                .post(post)
+                .content("DKJFkldfhdskalhgajklfg ukesagfhkjlasgjurelagrfhjagfhjksd fkjsfg jyf")
+                .build();
+    }
 }
