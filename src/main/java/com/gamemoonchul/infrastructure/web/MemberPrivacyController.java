e55039816eb46a2f40b45897ed9d6b721bb693d4
rookedsysc <rookedsysc36@gmail.com>
2024-05-26 14:54:55 +0900

    modify: uri privacy로 통일 및 HttpRequestMethod로 구분

--- src/main/java/com/gamemoonchul/infrastructure/web/MemberPrivacyController.java
+++ src/main/java/com/gamemoonchul/infrastructure/web/MemberPrivacyController.java
@@ -5,6 +5,7 @@ import com.gamemoonchul.common.annotation.MemberSession;
 import com.gamemoonchul.domain.entity.Member;
 import com.gamemoonchul.infrastructure.web.common.RestControllerWithEnvelopPattern;
 import com.gamemoonchul.infrastructure.web.dto.MemberResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberPrivacyController {
     private final MemberService memberService;
 
-    @PatchMapping("/agree")
-    public MemberResponseDto agreePrivacy(@MemberSession Member member) {
+    @PatchMapping
+    @Operation(description = "개인정보 처리방침 동의로 업데이트 하는 API")
+    public MemberResponseDto privacyAgree(@MemberSession Member member) {
         return memberService.privacyAgree(member);
     }
 
-    @GetMapping("/is-agreed")
+    @GetMapping
+    @Operation(description = "개인정보 처리방침 동의했는지 확인하는 API")
     public boolean isAgreedPrivacy(@MemberSession Member member) {
         return member.isPrivacyAgreed();
     }
