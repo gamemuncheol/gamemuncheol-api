6f9f8a2c5ad27d79025e58a2766f303e76b9e031
rookedsysc <rookedsysc36@gmail.com>
2024-06-02 19:39:00 +0900

    modify: 요구사항 변경에 따른 Comment Exception 추가 대댓글의 대댓글 비허용, 다른 PostId를 가진 Post 작성 불가 등

--- src/main/java/com/gamemoonchul/domain/status/PostStatus.java
+++ src/main/java/com/gamemoonchul/domain/status/PostStatus.java
@@ -13,7 +13,10 @@ public enum PostStatus implements ApiStatusIfs {
     POST_NOT_FOUND(6404, "게시글을 찾을 수 없습니다."),
     UNAUTHORIZED_REQUEST(6403, "권한없는 요청 입니다."),
     WRONG_MATCH_USER(6402, "잘못된 투표 유저 입니다."),
-    COMMENT_NOT_FOUND(6405, "댓글을 찾을 수 없습니다.");
+    COMMENT_NOT_FOUND(6405, "댓글을 찾을 수 없습니다."),
+    COMMENT_CANT_HAVE_GRANDMOTHER(6406, "대댓글 까지만 허용 됩니다."),
+    INVALID_REPLY(6407, "부모와 자식 댓글이 Post 동일한 Post에서 작성되지 않았습니다.")
+    ;
 
     private final Integer statusCode;
     private final String message;
