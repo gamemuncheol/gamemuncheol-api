209950fb96dd807b0e0e518f2cead74acbfa2423
rookedsysc <rookedsysc36@gmail.com>
2024-06-07 22:09:37 +0900

    modify: ratio 추가 및 아무런 값도 넣지 않을 경우 0이 들어가도록 설정

--- src/main/java/com/gamemoonchul/domain/model/dto/VoteRate.java
+++ src/main/java/com/gamemoonchul/domain/model/dto/VoteRate.java
@@ -1,27 +1,33 @@
 package com.gamemoonchul.domain.model.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class VoteRate {
     private Long matchUserId;
     private String nickname;
     private String championThumbnail;
     private Long voteOptionsId;
+    private Double ratio;
 
     @QueryProjection
     public VoteRate(
             Long matchUserId,
             String nickname,
             String championThumbnail,
-            Long voteOptionsId
+            Long voteOptionsId,
+            Long ratio
     ) {
         this.matchUserId = matchUserId;
         this.nickname = nickname;
         this.championThumbnail = championThumbnail;
         this.voteOptionsId = voteOptionsId;
+        this.ratio = ratio == null ? 0. : ratio;
+    }
+
+    public void setRatio(Double ratio) {
+        this.ratio = ratio;
     }
 }
