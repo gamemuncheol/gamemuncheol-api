5224febf588b85a69e6afb8d43ba17efeb407ff5
slbin-park <smpts00@naver.com>
2024-06-16 00:45:37 +0900

    feat: voteOptionDetail dto 추가

---
+++ src/main/java/com/gamemoonchul/infrastructure/web/dto/VoteOptionDetail.java
@@ -0,0 +1,24 @@
+package com.gamemoonchul.infrastructure.web.dto;
+
+import com.gamemoonchul.domain.entity.VoteOptions;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class VoteOptionDetail {
+    private Long matchUserId;
+    private String nickname;
+    private String championThumbnail;
+    private Long voteOptionsId;
+    private Double ratio;
+
+    public static VoteOptionDetail entityToResponse(VoteOptions entity) {
+        return VoteOptionDetail.builder()
+                .matchUserId(entity.getMatchUser().getId())
+                .nickname(entity.getMatchUser().getNickname())
+                .championThumbnail(entity.getMatchUser().getChampionThumbnail())
+                .voteOptionsId(entity.getId())
+                .build();
+    }
+}
