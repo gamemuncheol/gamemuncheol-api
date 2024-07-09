7a84722de95eacef11ba72ec83d00c85bb01d6f6
slbin-park <smpts00@naver.com>
2024-07-09 20:47:05 +0900

    feat: postId와 memberId로 vote 를 찾는 함수 추가

--- src/main/java/com/gamemoonchul/infrastructure/repository/VoteRepository.java
+++ src/main/java/com/gamemoonchul/infrastructure/repository/VoteRepository.java
@@ -5,9 +5,12 @@ import com.gamemoonchul.domain.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     void deleteAllByMember(Member member);
 
     List<Vote> findByMember(Member member);
+
+    Optional<Vote> findByPostIdAndMemberId(Long postId, Long memberId);
 }
