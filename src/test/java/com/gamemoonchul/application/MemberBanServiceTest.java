e8b1f528d29398d39973fb2930ec1f5e81c7ee0d
rookedsysc <rookedsysc36@gmail.com>
2024-07-11 17:23:30 +0900

    test: MemberBan Test 작성

---
+++ src/test/java/com/gamemoonchul/application/MemberBanServiceTest.java
@@ -0,0 +1,40 @@
+package com.gamemoonchul.application;
+
+import com.gamemoonchul.TestDataBase;
+import com.gamemoonchul.domain.entity.Member;
+import com.gamemoonchul.domain.entity.MemberBan;
+import com.gamemoonchul.domain.entity.MemberDummy;
+import com.gamemoonchul.infrastructure.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberBanServiceTest extends TestDataBase {
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberBanService memberBanService;
+
+    @Test
+    @DisplayName("Member Ban 후 Member Ban List 잘 받아와지는지 테스트")
+    void banTest() {
+        // given
+        Member member = MemberDummy.createWithId("홍길동");
+        Member banMember = MemberDummy.createWithId("김철수");
+        member = memberRepository.save(member);
+        banMember = memberRepository.save(banMember);
+
+        // when
+        memberBanService.ban(member, banMember.getId());
+
+        // then
+        List<MemberBan> bannedMember = memberBanService.bannedMembers(member.getId());
+        assertEquals(1, bannedMember.size());
+        assertEquals(bannedMember.get(0).getBanMember().getName(), banMember.getName());
+    }
+
+}
