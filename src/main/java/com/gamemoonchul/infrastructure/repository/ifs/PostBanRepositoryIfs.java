1e3667c3534525677cee24addde784102c45471f
rookedsysc <rookedsysc36@gmail.com>
2024-07-12 04:56:02 +0900

    feat: PostBan memberId로 찾는 쿼리 개발 Post N + 1 문제 발생 X

---
+++ src/main/java/com/gamemoonchul/infrastructure/repository/ifs/PostBanRepositoryIfs.java
@@ -0,0 +1,10 @@
+package com.gamemoonchul.infrastructure.repository.ifs;
+
+import com.gamemoonchul.domain.entity.Member;
+import com.gamemoonchul.domain.entity.PostBan;
+
+import java.util.List;
+
+public interface PostBanRepositoryIfs {
+    List<PostBan> searchByMemberId(Long memberId);
+}
