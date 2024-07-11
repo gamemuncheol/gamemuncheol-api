d28ac3fa265ccf5103e9225f4b301a7e7544c114
rookedsysc <rookedsysc36@gmail.com>
2024-07-11 19:58:51 +0900

    feat: Post 차단 기능 개발

---
+++ src/main/java/com/gamemoonchul/application/PostBanService.java
@@ -0,0 +1,22 @@
+package com.gamemoonchul.application;
+
+import com.gamemoonchul.domain.entity.Member;
+import com.gamemoonchul.domain.entity.Post;
+import com.gamemoonchul.domain.entity.PostBan;
+import com.gamemoonchul.infrastructure.repository.PostBanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostBanService {
+    private final PostBanRepository postBanRepository;
+
+    public void ban(Member member, Post post) {
+        PostBan postBan = PostBan.builder()
+                .banPost(post)
+                .member(member)
+                .build();
+        postBanRepository.save(postBan);
+    }
+}
