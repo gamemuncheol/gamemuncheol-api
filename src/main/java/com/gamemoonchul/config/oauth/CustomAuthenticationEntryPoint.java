207f901512dac1d655a38f50b1ac506cfcfd2a6e
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2024-07-28 16:34:43 +0900

    SpringSecurity 공통 에러 발생하는 부분 Stacktrace 한 줄씩 출력 될 수 있도록 수정 (#177)
    * refactor: SpringSecurity 공통 에러 발생하는 부분 Stacktrace 한 줄씩 출력 될 수 있도록 수정
    
    * feat: CustomEntryPoint NotFoundException 추가
    
    * fix
    
    * delete: 불필요하게 404 설정하는 코드 수정

--- src/main/java/com/gamemoonchul/config/oauth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/gamemoonchul/config/oauth/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.gamemoonchul.config.oauth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gamemoonchul.common.status.ApiStatus;
 import com.gamemoonchul.domain.status.Oauth2Status;
 import com.gamemoonchul.infrastructure.web.common.ApiResponse;
 import jakarta.servlet.ServletException;
@@ -27,8 +28,23 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         response.setContentType("application/json");
+
+        if (response.getStatus() == 404) {
+            handleNotFoundException(response);
+        } else {
+            handleAuthException(response, authException);
+        }
+    }
+
+    private void handleNotFoundException(HttpServletResponse response) throws IOException {
+        objectMapper.writeValue(response.getWriter(), ApiResponse.ERROR(ApiStatus.NOT_FOUND, null));
+    }
+
+    private void handleAuthException(HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        Arrays.stream(authException.getStackTrace()).map(StackTraceElement::toString).forEach(log::error);
         log.error(authException.getMessage() + "\n" + Arrays.toString(authException.getStackTrace()));
         objectMapper.writeValue(response.getWriter(), ApiResponse.ERROR(Oauth2Status.LOGIN_FAILED, authException.getMessage()));
     }
